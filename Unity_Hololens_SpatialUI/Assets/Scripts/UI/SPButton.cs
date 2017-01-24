1ec640cc774d4562da12b161d26106732efb18a1
Seelan <seelanv@gmail.com>
2017-01-24 16:16:40 -0500

    Started on test scene for conducting some UI tests

---
+++ Unity_Hololens_SpatialUI/Assets/Scripts/UI/SPButton.cs
@@ -0,0 +1,39 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SPButton : MonoBehaviour, IInputClickHandler, IFocusable
+{
+	public TextMesh _buttonText;
+	public Collider _buttonCollider;
+	
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void OnInputClicked(InputClickedEventData eventData)
+	{
+		//throw new NotImplementedException();
+	}
+
+	public void OnFocusEnter()
+	{
+		//throw new NotImplementedException();
+	}
+
+	public void OnFocusExit()
+	{
+		//throw new NotImplementedException();
+	}
+}
